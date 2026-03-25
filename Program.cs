a998aef7420f98088c42112abd036e98f336955f
EmAyyTee <HanzoSwitchMail@gmail.com>
2026-03-25 17:20:50 +0100

    Added a test case

--- Program.cs
+++ Program.cs
@@ -1,1 +1,43 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Reflection.Metadata;
+using APBD_Zadanie_Pierwsze.Models;
+using APBD_Zadanie_Pierwsze.Services;
+
+var rentalService = new RentalService();
+
+var user = new Student()
+{
+    Id = 1,
+    FirstName = "Jan",
+    LastName = "Kowalski"
+};
+
+var laptop = new Laptop()
+{
+    Id = 1,
+    Name = "Gaming Laptop",
+    Processor = "i7",
+    Graphics = "RTX 3000",
+    PricePerDay = 50
+};
+
+var allEquipment = new List<Equipement>()
+{
+    laptop
+};
+
+var rental = rentalService.Rent(user, laptop);
+
+Console.WriteLine($"Rented: {rental.RentedItem.Name}");
+Console.WriteLine($"User: {user.FUllName}");
+Console.WriteLine($"Max rentals: {user.MaxRentals}");
+    
+var availableAfterRent = rentalService.GetAvailableEquipment(allEquipment);
+Console.WriteLine($"Available after rent: {availableAfterRent.Count}");
+
+var fee = rentalService.ReturnEquipment(rental);
+
+
+Console.WriteLine($"Returned. Fee: {fee}");
+
+var availableAfterReturn = rentalService.GetAvailableEquipment(allEquipment);
+Console.WriteLine($"Available after return: {availableAfterReturn.Count}");
