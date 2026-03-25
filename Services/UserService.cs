09abc454c14c6aaecaa8ce63373d6ed20851d6ae
EmAyyTee <HanzoSwitchMail@gmail.com>
2026-03-25 20:01:00 +0100

    Created the method to add users

---
+++ Services/UserService.cs
@@ -0,0 +1,17 @@
+﻿using APBD_Zadanie_Pierwsze.Models;
+
+namespace APBD_Zadanie_Pierwsze.Services;
+
+public class UserService
+{
+    private int _nextId = 1;
+    private readonly List<User> _users = new();
+
+    public T AddUser<T>(T user) where T : User
+    {
+        user.Id = _nextId++;
+        _users.Add(user);
+        
+        return user;
+    }
+}
